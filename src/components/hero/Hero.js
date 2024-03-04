1a6a352dfd0544e88b29a7b0f4e1bc67f4333c8a
Satya Shah <88096065+satyashah@users.noreply.github.com>
2024-03-04 14:13:29 -0500

    modified:   src/App.css 	modified:   src/components/hero/Hero.css 	modified:   src/components/hero/Hero.js 	new file:   src/components/hero/git_dk.png 	new file:   src/components/hero/in_dk.png 	new file:   src/components/hero/mail_dk.png 	new file:   src/components/hero/md_dk.png

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,10 +2,14 @@
 import React from 'react';
 import './Hero.css'; // Import the CSS file
 import hero_img from "./hero_img.PNG"; // Import the image
+import git_dk from "./git_dk.png"; // Import the image
+import in_dk from "./in_dk.png"; // Import the image
+import mail_dk from "./mail_dk.png"; // Import the image
+import md_dk from "./md_dk.png"; // Import the image
 
 const Hero = () => {
     return (
-        <div className="hero-sep">
+        <div>
             <div className="hero">
                 <div className="hero-image">
                 </div>
@@ -17,10 +21,25 @@ const Hero = () => {
                             <p>Machine Learning // Full Stack</p>
                         </span>
                     </div>
+
+                    <div className="icon-grid2">
+                        <a href="link1"><div className="icon" style={{backgroundImage: `url(${git_dk})`}}></div></a>
+                        <a href="link2"><div className="icon" style={{backgroundImage: `url(${mail_dk})`}}></div></a>
+                        <a href="link3"><div className="icon" style={{backgroundImage: `url(${in_dk})`}}></div></a>
+                        <a href="link4"><div className="icon" style={{backgroundImage: `url(${md_dk})`}}></div></a>
+                    </div>
                 </div>
+                
             </div>
             
-            <div className="seperator"></div>
+            <div className="seperator">
+                <div className="icon-grid">
+                    <a href="link1"><div className="icon" style={{backgroundImage: `url(${git_dk})`}}></div></a>
+                    <a href="link2"><div className="icon" style={{backgroundImage: `url(${mail_dk})`}}></div></a>
+                    <a href="link3"><div className="icon" style={{backgroundImage: `url(${in_dk})`}}></div></a>
+                    <a href="link4"><div className="icon" style={{backgroundImage: `url(${md_dk})`}}></div></a>
+                </div>
+            </div>
         </div>
     );
 };
